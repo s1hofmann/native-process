cb3526c7c250b49bd30b2605645db90cb0a9635d
Simon Hofmann <simon.hofmann@consol.de>
2019-02-03 10:51:13 +0100

    Demo code requires timeout to work, need to investigate the exact reason

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 const { spawn } = require("child_process");
-const Process = require("./Process");
+const NativeProcess = require("./NativeProcess");
 
-const calc = spawn("/Applications/VLC.app/Contents/MacOS/VLC");
+const calc = spawn("/Applications/Calculator.app/Contents/MacOS/Calculator");
 console.log(calc.pid);
-const p = new Process();
-try {
-  const window = p.getMainWindow(calc.pid);
-  console.log(
-    `x: ${window.x}, y: ${window.y}, width: ${window.width}, height: ${
-      window.height
-    }`
-  );
-} catch (e) {
-  console.error(e);
-}
+const p = new NativeProcess();
+setTimeout(() => {
+  try {
+    const window = p.getMainWindow(calc.pid);
+    console.log(
+      `x: ${window.x},
+y: ${window.y},
+width: ${window.width},
+height: ${window.height}`
+    );
+  } catch (e) {
+    console.error(e);
+  }
+calc.kill();
+}, 200);
