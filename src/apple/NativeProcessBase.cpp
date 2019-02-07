bf5b57634d9065cdfaf5048cc78eea47aa6e601b
Simon Hofmann <simon.hofmann@consol.de>
2019-02-07 14:18:33 +0100

    Increased timeouts for application startup

--- src/apple/NativeProcessBase.cpp
+++ src/apple/NativeProcessBase.cpp
@@ -28,14 +28,12 @@ Napi::Value NativeProcessBase::getMainWindow(const Napi::CallbackInfo &info) {
     if (!info[0].IsNumber()) {
         Napi::Error::New(info.Env(), "Expected numeric process id.").ThrowAsJavaScriptException();
     }
-    Napi::Number nPid = info[0].As<Napi::Number>();
-    const int32_t pid = nPid.Int32Value();
-    Napi::Object windowBoundary = Napi::Object::New(info.Env());
+    const int32_t pid = info[0].As<Napi::Number>().Int32Value();
 
     // Create accessibility object using the process PID
     auto application = AXUIElementCreateApplication(pid);
     if (application == nullptr) {
-        Napi::TypeError::New(info.Env(), "Failed to create top-level accessibility object for process.").ThrowAsJavaScriptException();
+        Napi::Error::New(info.Env(), "Failed to create top-level accessibility object for process.").ThrowAsJavaScriptException();
     }
 
     AXUIElementRef mainWindow = nullptr;
@@ -43,17 +41,17 @@ Napi::Value NativeProcessBase::getMainWindow(const Napi::CallbackInfo &info) {
     for (int retry = 0; retry < 5; ++retry) {
         if (getMainWindowError == kAXErrorCannotComplete) {
             std::cout << "Failed to grab main window reference on try: " << retry << std::endl;
-            std::this_thread::sleep_for(std::chrono::milliseconds(200));
+            std::this_thread::sleep_for(std::chrono::seconds(1));
             getMainWindowError = AXUIElementCopyAttributeValue(application, kAXMainWindowAttribute, (CFTypeRef *) &mainWindow);
         } else {
             break;
         }
     }
     if (getMainWindowError != kAXErrorSuccess || mainWindow == nullptr) {
-        Napi::TypeError::New(info.Env(), "Failed to determine main window.").ThrowAsJavaScriptException();
+        return Napi::Object::New(info.Env());
+    } else {
+        return NativeWindowBase::constructor.New({ Napi::External<AXUIElementRef>::New(info.Env(), &mainWindow) });
     }
-
-    return NativeWindowBase::constructor.New({ Napi::External<AXUIElementRef>::New(info.Env(), &mainWindow) });
 }
 
 Napi::Value NativeProcessBase::getWindows(const Napi::CallbackInfo &info)
@@ -62,13 +60,13 @@ Napi::Value NativeProcessBase::getWindows(const Napi::CallbackInfo &info)
         Napi::Error::New(info.Env(), "Expected numeric process id.").ThrowAsJavaScriptException();
     }
     Napi::Number nPid = info[0].As<Napi::Number>();
-    const int32_t pid = nPid.Int32Value();
+    const int32_t pid = info[0].As<Napi::Number>().Int32Value();
     Napi::Array windowBoundaries;
 
     // Create accessibility object using the process PID
     auto application = AXUIElementCreateApplication(pid);
     if (application == nullptr) {
-        Napi::TypeError::New(info.Env(), "Failed to create top-level accessibility object for process.").ThrowAsJavaScriptException();
+        Napi::Error::New(info.Env(), "Failed to create top-level accessibility object for process.").ThrowAsJavaScriptException();
     }
 
     CFArrayRef windows = nullptr;
@@ -79,8 +77,8 @@ Napi::Value NativeProcessBase::getWindows(const Napi::CallbackInfo &info)
                                                             &windows);
     for (int retry = 0; retry < 5; ++retry) {
         if (getWindowsError == kAXErrorCannotComplete) {
-            std::cout << "Failed to grab main window reference on try: " << retry << std::endl;
-            std::this_thread::sleep_for(std::chrono::milliseconds(200));
+            std::cout << "Failed to grab window references on try: " << retry << std::endl;
+            std::this_thread::sleep_for(std::chrono::seconds(1));
             getWindowsError = AXUIElementCopyAttributeValues(application,
                                                             kAXWindowsAttribute,
                                                             0,
