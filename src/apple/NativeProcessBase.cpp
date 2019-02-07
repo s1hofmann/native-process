347ed5900975013f9e67d8bdde5cdd82e222f69b
Simon Hofmann <simon.hofmann@consol.de>
2019-02-07 10:07:49 +0100

    Changed methods to return native window instances

--- src/apple/NativeProcessBase.cpp
+++ src/apple/NativeProcessBase.cpp
@@ -1,4 +1,7 @@
 #include "NativeProcessBase.h"
+#include <iostream>
+#include <thread>
+#include <chrono>
 
 Napi::FunctionReference NativeProcessBase::constructor;
 
@@ -9,14 +12,9 @@ Napi::Object NativeProcessBase::Init(Napi::Env env, Napi::Object exports) {
         InstanceMethod("getWindows", &NativeProcessBase::getWindows),
     });
 
-    // Create a peristent reference to the class constructor. This will allow
-    // a function called on a class prototype and a function
-    // called on instance of a class to be distinguished from each other.
     constructor = Napi::Persistent(func);
-    // Call the SuppressDestruct() method on the static data prevent the calling
-    // to this destructor to reset the reference when the environment is no longer
-    // available.
     constructor.SuppressDestruct();
+
     exports.Set("NativeProcessBase", func);
     return exports;
 }
@@ -31,7 +29,7 @@ Napi::Value NativeProcessBase::getMainWindow(const Napi::CallbackInfo &info) {
         Napi::Error::New(info.Env(), "Expected numeric process id.").ThrowAsJavaScriptException();
     }
     Napi::Number nPid = info[0].As<Napi::Number>();
-    int32_t pid = nPid.Int32Value();
+    const int32_t pid = nPid.Int32Value();
     Napi::Object windowBoundary = Napi::Object::New(info.Env());
 
     // Create accessibility object using the process PID
@@ -41,43 +39,21 @@ Napi::Value NativeProcessBase::getMainWindow(const Napi::CallbackInfo &info) {
     }
 
     AXUIElementRef mainWindow = nullptr;
-    AXError getMainWindowError = AXUIElementCopyAttributeValue(application, kAXMainWindowAttribute, (CFTypeRef *)&mainWindow);
-    while (getMainWindowError == kAXErrorCannotComplete) {
-        getMainWindowError = AXUIElementCopyAttributeValue(application, kAXMainWindowAttribute, (CFTypeRef *)&mainWindow);
+    AXError getMainWindowError = AXUIElementCopyAttributeValue(application, kAXMainWindowAttribute, (CFTypeRef *) &mainWindow);
+    for (int retry = 0; retry < 5; ++retry) {
+        if (getMainWindowError == kAXErrorCannotComplete) {
+            std::cout << "Failed to grab main window reference on try: " << retry << std::endl;
+            std::this_thread::sleep_for(std::chrono::milliseconds(200));
+            getMainWindowError = AXUIElementCopyAttributeValue(application, kAXMainWindowAttribute, (CFTypeRef *) &mainWindow);
+        } else {
+            break;
+        }
     }
     if (getMainWindowError != kAXErrorSuccess || mainWindow == nullptr) {
         Napi::TypeError::New(info.Env(), "Failed to determine main window.").ThrowAsJavaScriptException();
     }
 
-    AXValueRef positionRef = nullptr;
-    AXValueRef sizeRef = nullptr;
-
-    if (AXUIElementCopyAttributeValue(mainWindow, kAXPositionAttribute, (CFTypeRef *)&positionRef) != kAXErrorSuccess || positionRef == nullptr) {
-        Napi::TypeError::New(info.Env(), "Failed to determine window position.").ThrowAsJavaScriptException();
-    }
-    if (AXUIElementCopyAttributeValue(mainWindow, kAXSizeAttribute, (CFTypeRef *)&sizeRef) != kAXErrorSuccess || sizeRef == nullptr) {
-        Napi::TypeError::New(info.Env(), "Failed to determine window size.").ThrowAsJavaScriptException();
-    }
-
-    CGPoint positionValue;
-    CGSize sizeValue;
-
-    if (!AXValueGetValue(positionRef, kAXValueTypeCGPoint, &positionValue)) {
-        Napi::TypeError::New(info.Env(), "Failed to unpack window position.").ThrowAsJavaScriptException();
-    }
-    if (!AXValueGetValue(sizeRef, kAXValueTypeCGSize, &sizeValue)) {
-        Napi::TypeError::New(info.Env(), "Failed to unpack window size.").ThrowAsJavaScriptException();
-    }
-
-    windowBoundary.Set("x", positionValue.x);
-    windowBoundary.Set("y", positionValue.y);
-    windowBoundary.Set("width", sizeValue.width);
-    windowBoundary.Set("height", sizeValue.height);
-
-    CFRetain(mainWindow);
-    CFRelease(application);
-
-    return windowBoundary;
+    return NativeWindowBase::constructor.New({ Napi::External<AXUIElementRef>::New(info.Env(), &mainWindow) });
 }
 
 Napi::Value NativeProcessBase::getWindows(const Napi::CallbackInfo &info)
@@ -86,7 +62,7 @@ Napi::Value NativeProcessBase::getWindows(const Napi::CallbackInfo &info)
         Napi::Error::New(info.Env(), "Expected numeric process id.").ThrowAsJavaScriptException();
     }
     Napi::Number nPid = info[0].As<Napi::Number>();
-    int32_t pid = nPid.Int32Value();
+    const int32_t pid = nPid.Int32Value();
     Napi::Array windowBoundaries;
 
     // Create accessibility object using the process PID
@@ -96,11 +72,25 @@ Napi::Value NativeProcessBase::getWindows(const Napi::CallbackInfo &info)
     }
 
     CFArrayRef windows = nullptr;
-    AXUIElementCopyAttributeValues(application,
-                                   kAXWindowsAttribute,
-                                   0,
-                                   1024,
-                                   &windows);
+    AXError getWindowsError = AXUIElementCopyAttributeValues(application,
+                                                            kAXWindowsAttribute,
+                                                            0,
+                                                            NativeProcessBase::MAX_WINDOWS,
+                                                            &windows);
+    for (int retry = 0; retry < 5; ++retry) {
+        if (getWindowsError == kAXErrorCannotComplete) {
+            std::cout << "Failed to grab main window reference on try: " << retry << std::endl;
+            std::this_thread::sleep_for(std::chrono::milliseconds(200));
+            getWindowsError = AXUIElementCopyAttributeValues(application,
+                                                            kAXWindowsAttribute,
+                                                            0,
+                                                            NativeProcessBase::MAX_WINDOWS,
+                                                            &windows);
+            getWindowsError = AXUIElementCopyAttributeValue(application, kAXMainWindowAttribute, (CFTypeRef *) &windows);
+        } else {
+            break;
+        }
+    }
 
     if (windows == nullptr) {
         Napi::TypeError::New(info.Env(), "No windows associated with process.").ThrowAsJavaScriptException();
--- src/apple/NativeProcessBase.h
+++ src/apple/NativeProcessBase.h
@@ -3,13 +3,17 @@
 #include <napi.h>
 #include <ApplicationServices/ApplicationServices.h>
 
+#include "NativeWindowBase.h"
+
 class NativeProcessBase : public Napi::ObjectWrap<NativeProcessBase> {
   public:
+    static Napi::FunctionReference constructor;
     static Napi::Object Init(Napi::Env env, Napi::Object exports);
     NativeProcessBase(const Napi::CallbackInfo &info);
 
+    static constexpr long MAX_WINDOWS = 1024;
+
   private:
-    static Napi::FunctionReference constructor;
     Napi::Value getMainWindow(const Napi::CallbackInfo &info);
     Napi::Value getWindows(const Napi::CallbackInfo &info);
 };
